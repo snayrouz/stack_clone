920c9cb37901819824b813f3294d1bc760c21026
snayrouz <samuel.nayrouz@gmail.com>
2017-11-13 16:58:12 -0700

    Add relationships

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
+  has_many :comments, as: :commentable
+  has_many :votes, as: :votable
+  has_many :answers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :questions
-  
+  has_many :answers
+  has_many :votes
+  has_many :comments
+
   validates :email, uniqueness: true, presence: true
   validates :username, uniqueness: true, presence: true
-  validates :password, uniqueness: true, presence: true
 end
