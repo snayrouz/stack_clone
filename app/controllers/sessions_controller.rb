037c5794cc5f467be6436b87a46b4c2e1dd9f248
snayrouz <samuel.nayrouz@gmail.com>
2017-11-13 16:20:59 -0700

    Add new form view and partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,33 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+    if request.xhr?
+      render(partial: 'new', object: @user)
+    end
   end
 
   def create
+    @user = User.find_by(username: session_params[:username])
+    if @user != nil
+      if @user.authenticate(session_params[:password])
+        session[:user_id] = @user.id
+        redirect_to questions_path
+      else
+        redirect_to login_path
+      end
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
+
+  private
+
+    def session_params
+      params.require(:user).permit(:username, :password)
+    end
 end
