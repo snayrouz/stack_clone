eec9fa0480718913d995aa91caa7e3d8e679fc19
snayrouz <samuel.nayrouz@gmail.com>
2017-11-13 17:56:03 -0700

    Configured shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:username) }
-  it { should validate_presence_of(:password_digest) }
 
   context "as a user" do
 
