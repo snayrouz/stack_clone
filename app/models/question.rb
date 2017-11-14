671a940134feea18d86908bc496f3b3202a66793
snayrouz <samuel.nayrouz@gmail.com>
2017-11-13 18:02:27 -0700

    Add relationships and migrations for tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,6 @@ class Question < ApplicationRecord
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
   has_many :answers
+  has_many :taggings
+  has_many :tags, through: :taggings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113235600) do
+ActiveRecord::Schema.define(version: 20171114005930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,21 @@ ActiveRecord::Schema.define(version: 20171113235600) do
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id"
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_taggings_on_question_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -69,5 +84,7 @@ ActiveRecord::Schema.define(version: 20171113235600) do
   add_foreign_key "answers", "users"
   add_foreign_key "comments", "users"
   add_foreign_key "questions", "users"
+  add_foreign_key "taggings", "questions"
+  add_foreign_key "taggings", "tags"
   add_foreign_key "votes", "users"
 end
