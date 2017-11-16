23849e15fa339621f935a070bf5f2d0c8031123d
snayrouz <samuel.nayrouz@gmail.com>
2017-11-16 13:13:13 -0700

    Add routes and requirements to applicationjs

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-
+  post 'tokens' => "tokens#create"
   resources :users
 
   resources :questions do
