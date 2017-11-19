3dc898d83262a909b124cec7652f32cc8b2ae133
snayrouz <samuel.nayrouz@gmail.com>
2017-11-19 09:30:03 -0700

    Tag spec model pass

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tag) { Tag.create!(name: 'ruby') }
+
+  it { should have_many(:taggings) }
+  it { should have_many(:questions) }
+
+  it "should have a name" do
+    expect(tag.name).to eq("ruby")
+  end
 end
